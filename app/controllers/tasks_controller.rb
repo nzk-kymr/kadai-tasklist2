04d53a84ec8cb58eb31f59b8da36a4cc3909298b
kymr.nzk <kymrxnzk@gmail.com>
2020-02-13 12:25:47 +0000

    users contents

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     
-    @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(3)
     
   end
 
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -54,7 +54,7 @@ class TasksController < ApplicationController
   # Strong Parameter
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
   def task_params
     params.require(:task).permit(:content,:status)
